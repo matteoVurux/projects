d28b8214191b69d7678a75f0c5d91c30e43078ea
Matteo Calcina <122792207+matteoVurux@users.noreply.github.com>
2023-01-17 17:47:21 +0100

    Delete football directory

--- football/football-app/src/app/shared/service/snackbar.service.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface SnackbarServiceInterface {
-    /**
-     * Open and show the snackbar
-     * @param content content shown by snackbar
-     * @param action action available in the snackbar
-     * @param duration duration of the snackbar
-     */
-    open(content: string, action: string, duration: number): void
-}
