26d2e9c42a158d1efbddd858af766ea8e23f5f70
Matteo Calcina <122792207+matteoVurux@users.noreply.github.com>
2023-01-17 15:58:45 +0100

    Delete football directory

--- football/football-server/src/shared/interface/player.interface.ts
+++
@@ -1,9 +0,0 @@
-import { Team } from '../entity/team.entity';
-
-export interface PlayerInterface {
-    id?: string;
-    name: string;
-    team_defender?: Team;
-    team_striker?: Team;
-    points?:number;
-}
